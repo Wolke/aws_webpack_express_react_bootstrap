617cf4d0efe663b0884c059a190923a95e99685e
Wolke <wolkesau@gmail.com>
2015-12-23 16:36:30 +0800

    begin

---
+++ public/assets/js/bootstrap.js
@@ -0,0 +1,4 @@
+// @koala-prepend "../../bower_components/jquery/dist/jquery.js"
+// @koala-prepend "../../bower_components/bootstrap/js/transition.js"
+// @koala-prepend "../../bower_components/jasny-bootstrap/js/offcanvas.js"
+
