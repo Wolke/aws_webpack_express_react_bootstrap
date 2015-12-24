e4319116b777595762174039177962c41b50b35f
Wolke <wolkesau@gmail.com>
2015-12-25 00:00:44 +0800

    p

--- public/build/html.js
+++ public/build/html.js
@@ -1,8 +1,1 @@
-webpackJsonp([0,3],[
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = __webpack_require__.p + "index.html";
-
-/***/ }
-]);
+webpackJsonp([3,0],[function(n,p,e){n.exports=e.p+"index.html"}]);
